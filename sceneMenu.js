438945cb327e473ee3b5bc49911ca51a0523a085
Lenovo <orvinoray@gmail.com>
2025-06-11 01:21:00 +0700

    update bug

--- sceneMenu.js
+++ sceneMenu.js
@@ -1,207 +1,225 @@
-var sceneMenu = new Phaser.Class({
-  Extends: Phaser.Scene,
-  initialize: function () {
-    Phaser.Scene.call(this, { key: "sceneMenu" });
-  },
+      var sceneMenu = new Phaser.Class({
+        Extends: Phaser.Scene,
+        initialize: function () {
+          Phaser.Scene.call(this, { key: "sceneMenu" });
+        },
 
-  init() {},
+        init() {},
 
-  preload() {
-    this.load.image("bg_start", "images/bg_start.png");
-    this.load.image("btn_play", "images/btn_play.png");
-    this.load.image("title_game", "images/title_game.png");
-    this.load.image("panel_skor", "images/panel_skor.png");
+        preload() {
+          // Fixed file paths - remove leading slashes and use relative paths
+          this.load.image("bg_start", "./images/bg_start.png");
+          this.load.image("btn_play", "./images/btn_play.png");
+          this.load.image("title_game", "./images/title_game.png");
+          this.load.image("panel_skor", "./images/panel_skor.png");
 
-    this.load.audio("snd_ambience", "../audio/ambience.mp3");
-    this.load.audio("snd_touch", "../audio/touch.mp3");
-    this.load.audio("snd_transisi_menu", "../audio/transisi_menu.mp3");
+          this.load.audio("snd_ambience", "./audio/ambience.mp3");
+          this.load.audio("snd_touch", "./audio/touch.mp3");
+          this.load.audio("snd_transisi_menu", "./audio/transisi_menu.mp3");
 
-    this.load.spritesheet("sps_mummy", "sprite/mummy37x45.png", {
-      frameWidth: 37,
-      frameHeight: 45,
-    });
-  },
+          this.load.spritesheet("sps_mummy", "./sprite/mummy37x45.png", {
+            frameWidth: 37,
+            frameHeight: 45,
+          });
 
-  create: function () {
-    mummy = this.add.sprite(1024 / 2, 768 - 170, "sps_mummy");
-    mummy.setDepth(10);
-    mummy.setScale(3);
-    this.anims.create({
-      key: "walk",
-      frames: this.anims.generateFrameNumbers("sps_mummy", {
-        start: 0,
-        end: 17,
-      }),
-      frameRate: 16,
-    });
-    mummy.play({ key: "walk", repeat: -1 });
+          // Add error handling for failed loads
+          this.load.on('filecomplete', function (key, type, data) {
+            console.log('Loaded:', key, type);
+          });
 
-    X_POSITION = {
-      LEFT: 0,
-      CENTER: this.sys.game.canvas.width / 2,
-      RIGHT: this.sys.game.canvas.width,
-    };
+          this.load.on('loaderror', function (file) {
+            console.error('Failed to load:', file.src);
+          });
+        },
 
-    Y_POSITION = {
-      TOP: 0,
-      CENTER: this.sys.game.canvas.height / 2,
-      BOTTOM: this.sys.game.canvas.height,
-    };
+        create: function () {
+          var mummy = this.add.sprite(1024 / 2, 768 - 170, "sps_mummy");
+          mummy.setDepth(10);
+          mummy.setScale(3);
+          this.anims.create({
+            key: "walk",
+            frames: this.anims.generateFrameNumbers("sps_mummy", {
+              start: 0,
+              end: 17,
+            }),
+            frameRate: 16,
+          });
+          mummy.play({ key: "walk", repeat: -1 });
 
-    this.add.image(X_POSITION.CENTER, Y_POSITION.CENTER, "bg_start");
+          X_POSITION = {
+            LEFT: 0,
+            CENTER: this.sys.game.canvas.width / 2,
+            RIGHT: this.sys.game.canvas.width,
+          };
 
-    var btnPlay = this.add.image(
-      X_POSITION.CENTER,
-      Y_POSITION.CENTER,
-      "btn_play"
-    );
-    if (snd_ambience == null) {
-      snd_ambience = this.sound.add("snd_ambience");
-      snd_ambience.loop = true;
-      snd_ambience.setVolume(0.35);
-      snd_ambience.play();
-    }
+          Y_POSITION = {
+            TOP: 0,
+            CENTER: this.sys.game.canvas.height / 2,
+            BOTTOM: this.sys.game.canvas.height,
+          };
 
-    this.snd_touch = this.sound.add("snd_touch");
-    var snd_transisi = this.sound.add("snd_transisi_menu");
+          this.add.image(X_POSITION.CENTER, Y_POSITION.CENTER, "bg_start");
 
-    var skorTertinggi = localStorage["highscore"] || 0;
+          var btnPlay = this.add.image(
+            X_POSITION.CENTER,
+            Y_POSITION.CENTER,
+            "btn_play"
+          );
+          
+          if (snd_ambience == null) {
+            snd_ambience = this.sound.add("snd_ambience");
+            snd_ambience.loop = true;
+            snd_ambience.setVolume(0.35);
+            snd_ambience.play();
+          }
 
-    var panelSkor = this.add.image(1024 / 2, 768 - 120, "panel_skor");
-    panelSkor.setOrigin(0.5);
-    panelSkor.setDepth(10);
-    // Hapus setAlpha(0) atau ubah menjadi setAlpha(1)
-    panelSkor.setAlpha(1);
+          this.snd_touch = this.sound.add("snd_touch");
+          var snd_transisi = this.sound.add("snd_transisi_menu");
 
-    var lblSkor = this.add.text(
-      panelSkor.x + 25,
-      panelSkor.y,
-      "High Score : " + skorTertinggi
-    );
-    lblSkor.setOrigin(0.5);
-    lblSkor.setDepth(10);
-    lblSkor.setFontSize(25);
-    lblSkor.setTint(0xff732e);
+          // Use sessionStorage as fallback for GitHub Pages
+          var skorTertinggi = 0;
+          try {
+            skorTertinggi = localStorage.getItem("highscore") || 0;
+          } catch (e) {
+            console.warn("localStorage not available, using sessionStorage");
+            skorTertinggi = sessionStorage.getItem("highscore") || 0;
+          }
 
-    this.titleGame = this.add.image(1024 / 2, 200, "title_game");
-    this.titleGame.setDepth(10);
+          var panelSkor = this.add.image(1024 / 2, 768 - 120, "panel_skor");
+          panelSkor.setOrigin(0.5);
+          panelSkor.setDepth(10);
+          panelSkor.setAlpha(1);
 
-    this.titleGame.y -= 384;
+          var lblSkor = this.add.text(
+            panelSkor.x + 25,
+            panelSkor.y,
+            "High Score : " + skorTertinggi
+          );
+          lblSkor.setOrigin(0.5);
+          lblSkor.setDepth(10);
+          lblSkor.setFontSize(25);
+          lblSkor.setTint(0xff732e);
 
-    var diz = this;
+          this.titleGame = this.add.image(1024 / 2, 200, "title_game");
+          this.titleGame.setDepth(10);
+          this.titleGame.y -= 384;
 
-    this.tweens.add({
-      targets: diz.titleGame,
-      ease: "Bounce.easeOut",
-      duration: 750,
-      delay: 250,
-      y: 200,
-      onComplete: function () {
-        snd_transisi.play();
-      },
-    });
+          var diz = this;
 
-    btnPlay.setScale(0);
+          this.tweens.add({
+            targets: diz.titleGame,
+            ease: "Bounce.easeOut",
+            duration: 750,
+            delay: 250,
+            y: 200,
+            onComplete: function () {
+              snd_transisi.play();
+            },
+          });
 
-    this.tweens.add({
-      targets: btnPlay,
-      ease: "Back",
-      duration: 500,
-      delay: 750,
-      scaleX: 1,
-      scaleY: 1,
-    });
+          btnPlay.setScale(0);
 
-    this.titleGame.setScale(0);
+          this.tweens.add({
+            targets: btnPlay,
+            ease: "Back",
+            duration: 500,
+            delay: 750,
+            scaleX: 1,
+            scaleY: 1,
+          });
 
-    this.tweens.add({
-      targets: diz.titleGame,
-      ease: "Elastic",
-      duration: 750,
-      delay: 1000,
-      scaleX: 1,
-      scaleY: 1,
-    });
+          this.titleGame.setScale(0);
 
-    // Tambahkan animasi fade-in untuk panel skor agar lebih menarik
-    panelSkor.setAlpha(0);
-    this.tweens.add({
-      targets: panelSkor,
-      ease: "Power2",
-      duration: 500,
-      delay: 1250,
-      alpha: 1,
-    });
+          this.tweens.add({
+            targets: diz.titleGame,
+            ease: "Elastic",
+            duration: 750,
+            delay: 1000,
+            scaleX: 1,
+            scaleY: 1,
+          });
 
-    // Tambahkan animasi fade-in untuk label skor juga
-    lblSkor.setAlpha(0);
-    this.tweens.add({
-      targets: lblSkor,
-      ease: "Power2",
-      duration: 500,
-      delay: 1500,
-      alpha: 1,
-    });
+          panelSkor.setAlpha(0);
+          this.tweens.add({
+            targets: panelSkor,
+            ease: "Power2",
+            duration: 500,
+            delay: 1250,
+            alpha: 1,
+          });
 
-    this.input.on(
-      "gameobjectover",
-      function (pointer, gameObject) {
-        console.log("Scene Menu | Object Over");
-        if (!btnClicked) return;
-        if (gameObject == btnPlay) {
-          btnPlay.setTint(0x616161);
-        }
-      },
-      this
-    );
-    this.input.on(
-      "gameobjectout",
-      function (pointer, gameObject) {
-        console.log("Scene Menu | Object Out");
-        if (!btnClicked) return;
-        if (gameObject == btnPlay) {
-          btnPlay.setTint(0xffffff);
-          btnClicked = true;
-        }
-      },
-      this
-    );
-    this.input.on(
-      "gameobjectdown",
-      function (pointer, gameObject) {
-        console.log("Scene Menu | Object Click");
-        if (gameObject == btnPlay) {
-          btnPlay.setTint(0x616161);
-          btnClicked = true;
-        }
-      },
-      this
-    );
-    this.input.on(
-      "gameobjectup",
-      function (pointer, gameObject) {
-        console.log("Scene Menu | Object End Click");
-        if (gameObject == btnPlay) {
-          btnPlay.setTint(0xffffff);
-          this.scene.start("scenePlay");
-          this.snd_touch.play();
-        }
-      },
-      this
-    );
-    this.input.on(
-      "pointerup",
-      function (pointer, gameObject) {
-        console.log("Scene Menu | Mouse Up");
-        btnClicked = false;
-      },
-      this
-    );
+          lblSkor.setAlpha(0);
+          this.tweens.add({
+            targets: lblSkor,
+            ease: "Power2",
+            duration: 500,
+            delay: 1500,
+            alpha: 1,
+          });
 
-    btnPlay.setInteractive();
+          var btnClicked = false;
 
-    var btnClicked = false;
-  },
+          this.input.on(
+            "gameobjectover",
+            function (pointer, gameObject) {
+              console.log("Scene Menu | Object Over");
+              if (!btnClicked) return;
+              if (gameObject == btnPlay) {
+                btnPlay.setTint(0x616161);
+              }
+            },
+            this
+          );
+          
+          this.input.on(
+            "gameobjectout",
+            function (pointer, gameObject) {
+              console.log("Scene Menu | Object Out");
+              if (!btnClicked) return;
+              if (gameObject == btnPlay) {
+                btnPlay.setTint(0xffffff);
+                btnClicked = true;
+              }
+            },
+            this
+          );
+          
+          this.input.on(
+            "gameobjectdown",
+            function (pointer, gameObject) {
+              console.log("Scene Menu | Object Click");
+              if (gameObject == btnPlay) {
+                btnPlay.setTint(0x616161);
+                btnClicked = true;
+              }
+            },
+            this
+          );
+          
+          this.input.on(
+            "gameobjectup",
+            function (pointer, gameObject) {
+              console.log("Scene Menu | Object End Click");
+              if (gameObject == btnPlay) {
+                btnPlay.setTint(0xffffff);
+                this.scene.start("scenePlay");
+                this.snd_touch.play();
+              }
+            },
+            this
+          );
+          
+          this.input.on(
+            "pointerup",
+            function (pointer, gameObject) {
+              console.log("Scene Menu | Mouse Up");
+              btnClicked = false;
+            },
+            this
+          );
 
-  update() {},
-});
+          btnPlay.setInteractive();
+        },
+
+        update() {},
+      });
--- scenePlay.js
+++ scenePlay.js
@@ -1,224 +1,225 @@
-var scenePlay = new Phaser.Class({
-  Extends: Phaser.Scene,
-  initialize: function () {
-    Phaser.Scene.call(this, { key: "scenePlay" });
-  },
-  init: function () {},
-  preload: function () {
-    this.load.image("chara", "images/chara.png");
-    this.load.image("fg_loop_back", "images/fg_loop_back.png");
-    this.load.image("fg_loop", "images/fg_loop.png");
-    this.load.image("obstc", "images/obstc.png");
-    this.load.image("panel_skor", "images/panel_skor.png");
+      var scenePlay = new Phaser.Class({
+        Extends: Phaser.Scene,
+        initialize: function () {
+          Phaser.Scene.call(this, { key: "scenePlay" });
+        },
+        init: function () {},
+        preload: function () {
+          // Fixed file paths
+          this.load.image("chara", "./images/chara.png");
+          this.load.image("fg_loop_back", "./images/fg_loop_back.png");
+          this.load.image("fg_loop", "./images/fg_loop.png");
+          this.load.image("obstc", "./images/obstc.png");
+          this.load.image("panel_skor", "./images/panel_skor.png");
 
-    this.load.audio("snd_dead", "../audio/dead.mp3");
-    this.load.audio("snd_klik_1", "../audio/klik_1.mp3");
-    this.load.audio("snd_klik_2", "../audio/klik_2.mp3");
-    this.load.audio("snd_klik_3", "../audio/klik_3.mp3");
-  },
-  create: function () {
-    this.background = [];
+          this.load.audio("snd_dead", "./audio/dead.mp3");
+          this.load.audio("snd_klik_1", "./audio/klik_1.mp3");
+          this.load.audio("snd_klik_2", "./audio/klik_2.mp3");
+          this.load.audio("snd_klik_3", "./audio/klik_3.mp3");
 
-    this.isGameRunning = false;
-    this.isGameOver = false; // Tambahkan flag untuk game over
+          // Add error handling
+          this.load.on('loaderror', function (file) {
+            console.error('Failed to load:', file.src);
+          });
+        },
+        create: function () {
+          this.background = [];
+          this.isGameRunning = false;
+          this.isGameOver = false;
 
-    this.chara = this.add.image(130, 768 / 2, "chara");
-    this.chara.setDepth(3);
+          this.chara = this.add.image(130, 768 / 2, "chara");
+          this.chara.setDepth(3);
+          this.chara.setScale(0);
 
-    this.chara.setScale(0);
+          this.timerHalangan = 0;
+          this.halangan = [];
+          this.score = 0;
 
-    this.timerHalangan = 0;
-    this.halangan = [];
+          this.panel_score = this.add.image(1024 / 2, 60, "panel_skor");
+          this.panel_score.setOrigin(0.5);
+          this.panel_score.setDepth(10);
+          this.panel_score.setAlpha(0.8);
 
-    this.score = 0;
+          this.label_score = this.add.text(
+            this.panel_score.x + 25,
+            this.panel_score.y,
+            this.score
+          );
+          this.label_score.setOrigin(0.5);
+          this.label_score.setDepth(10);
+          this.label_score.setFontSize(30);
+          this.label_score.setTint(0xff732e);
 
-    this.panel_score = this.add.image(1024 / 2, 60, "panel_skor");
-    this.panel_score.setOrigin(0.5);
-    this.panel_score.setDepth(10);
-    this.panel_score.setAlpha(0.8);
+          this.snd_dead = this.sound.add("snd_dead");
 
-    this.label_score = this.add.text(
-      this.panel_score.x + 25,
-      this.panel_score.y,
-      this.score
-    );
-    this.label_score.setOrigin(0.5);
-    this.label_score.setDepth(10);
-    this.label_score.setFontSize(30);
-    this.label_score.setTint(0xff732e);
+          this.snd_click = [];
+          this.snd_click.push(this.sound.add("snd_klik_1"));
+          this.snd_click.push(this.sound.add("snd_klik_2"));
+          this.snd_click.push(this.sound.add("snd_klik_3"));
 
-    this.snd_dead = this.sound.add("snd_dead");
+          for (let i = 0; i < this.snd_click.length; i++) {
+            this.snd_click[i].setVolume(0.5);
+          }
 
-    this.snd_click = [];
-    this.snd_click.push(this.sound.add("snd_klik_1"));
-    this.snd_click.push(this.sound.add("snd_klik_2"));
-    this.snd_click.push(this.sound.add("snd_klik_3"));
+          this.gameOver = function () {
+            var highScore = 0;
+            try {
+              highScore = localStorage.getItem("highscore") || 0;
+              if (this.score > highScore) {
+                localStorage.setItem("highscore", this.score);
+              }
+            } catch (e) {
+              console.warn("localStorage not available, using sessionStorage");
+              highScore = sessionStorage.getItem("highscore") || 0;
+              if (this.score > highScore) {
+                sessionStorage.setItem("highscore", this.score);
+              }
+            }
+            this.scene.start("sceneMenu");
+          };
 
-    for (let i = 0; i < this.snd_click.length; i++) {
-      this.snd_click[i].setVolume(0.5);
-    }
+          var myScene = this;
+          var bg_x = 1366 / 2;
 
-    // Pindahkan gameOver ke dalam fungsi terpisah dan perbaiki referensi
-    this.gameOver = function () {
-      let highScore = localStorage["highscore"] || 0;
+          for (let i = 0; i < 2; i++) {
+            var bg_awal = [];
 
-      if (this.score > highScore) {
-        localStorage["highscore"] = this.score;
-      }
-      this.scene.start("sceneMenu");
-    };
+            var BG = this.add.image(bg_x, 768 / 2, "fg_loop_back");
+            var FG = this.add.image(bg_x, 768 / 2, "fg_loop");
 
-    var myScene = this;
-    var bg_x = 1366 / 2;
+            BG.setData("kecepatan", 2);
+            FG.setData("kecepatan", 4);
+            FG.setDepth(2);
 
-    for (let i = 0; i < 2; i++) {
-      var bg_awal = [];
+            bg_awal.push(BG);
+            bg_awal.push(FG);
 
-      var BG = this.add.image(bg_x, 768 / 2, "fg_loop_back");
-      var FG = this.add.image(bg_x, 768 / 2, "fg_loop");
+            this.background.push(bg_awal);
 
-      BG.setData("kecepatan", 2);
-      FG.setData("kecepatan", 4);
-      FG.setDepth(2);
+            bg_x += 1366;
+          }
 
-      bg_awal.push(BG);
-      bg_awal.push(FG);
+          this.tweens.add({
+            delay: 250,
+            targets: this.chara,
+            ease: "Back.Out",
+            duration: 500,
+            scaleX: 1,
+            scaleY: 1,
+            onComplete: function () {
+              myScene.isGameRunning = true;
+            },
+          });
 
-      this.background.push(bg_awal);
+          this.input.on(
+            "pointerup",
+            function (pointer, currentlyOver) {
+              if (!this.isGameRunning || this.isGameOver) return;
 
-      bg_x += 1366;
-    }
+              this.snd_click[Math.floor((Math.random() * 3))].play();
 
-    this.tweens.add({
-      delay: 250,
-      targets: this.chara,
-      ease: "Back.Out",
-      duration: 500,
-      scaleX: 1,
-      scaleY: 1,
-      onComplete: function () {
-        myScene.isGameRunning = true;
-      },
-    });
+              this.charaTweens = this.tweens.add({
+                targets: this.chara,
+                ease: "Power1",
+                duration: 750,
+                y: this.chara.y + 200,
+              });
+            },
+            this
+          );
+        },
+        update: function () {
+          if (this.isGameOver) return;
 
-    this.input.on(
-      "pointerup",
-      function (pointer, currentlyOver) {
-        if (!this.isGameRunning || this.isGameOver) return; // Tambahkan check isGameOver
+          if (this.isGameRunning) {
+            this.chara.y -= 5;
 
-        this.snd_click[Math.floor((Math.random() * 2))].play();
+            if (this.chara.y > 690) this.chara.y = 690;
+            
+            if (this.chara.y < 50) {
+              this.handleGameOver();
+              return;
+            }
+          }
 
-        this.charaTweens = this.tweens.add({
-          targets: this.chara,
-          ease: "Power1",
-          duration: 750,
-          y: this.chara.y + 200,
-        });
-      },
-      this
-    );
-  },
-  update: function () {
-    // Hentikan semua update jika game over
-    if (this.isGameOver) return;
+          for (let i = 0; i < this.background.length; i++) {
+            for (var j = 0; j < this.background[i].length; j++) {
+              this.background[i][j].x -= this.background[i][j].getData("kecepatan");
 
-    if (this.isGameRunning) {
-      this.chara.y -= 5;
+              if (this.background[i][j].x <= -(1366 / 2)) {
+                var diff = this.background[i][j].x + 1366 / 2;
+                this.background[i][j].x = 1366 + 1366 / 2 + diff;
+              }
+            }
+          }
 
-      if (this.chara.y > 690) this.chara.y = 690;
-      
-      // Pindahkan pengecekan batas atas ke sini dan tambahkan return
-      if (this.chara.y < 50) {
-        this.handleGameOver();
-        return; // Keluar dari update setelah game over
-      }
-    }
+          if (this.timerHalangan == 0) {
+            var acak_y = Math.floor(Math.random() * 680 + 60);
+            var halanganBaru = this.add.image(1500, acak_y, "obstc");
 
-    for (let i = 0; i < this.background.length; i++) {
-      for (var j = 0; j < this.background[i].length; j++) {
-        this.background[i][j].x -= this.background[i][j].getData("kecepatan");
+            halanganBaru.setOrigin(0, 0);
+            halanganBaru.setData("status_aktif", true);
+            halanganBaru.setData("kecepatan", Math.floor(Math.random() * 15 + 10));
 
-        if (this.background[i][j].x <= -(1366 / 2)) {
-          var diff = this.background[i][j].x + 1366 / 2;
+            this.halangan.push(halanganBaru);
+            this.timerHalangan = Math.floor(Math.random() * 50 + 10);
+            halanganBaru.setDepth(3);
+          }
 
-          this.background[i][j].x = 1366 + 1366 / 2 + diff;
+          for (let i = this.halangan.length - 1; i >= 0; i--) {
+            this.halangan[i].x -= this.halangan[i].getData("kecepatan");
+
+            if (this.halangan[i].x < -200) {
+              this.halangan[i].destroy();
+              this.halangan.splice(i, 1);
+              break;
+            }
+          }
+
+          this.timerHalangan--;
+
+          for (var i = this.halangan.length - 1; i >= 0; i--) {
+            if (
+              this.chara.x > this.halangan[i].x + 50 &&
+              this.halangan[i].getData("status_aktif") == true
+            ) {
+              this.halangan[i].setData("status_aktif", false);
+              this.score++;
+              this.label_score.setText(this.score);
+            }
+          }
+
+          for (let i = this.halangan.length - 1; i >= 0; i--) {
+            if (
+              this.chara.getBounds().contains(this.halangan[i].x, this.halangan[i].y)
+            ) {
+              this.halangan[i].setData("status_aktif", false);
+              this.handleGameOver();
+              break;
+            }
+          }
+        },
+        
+        handleGameOver: function() {
+          this.isGameRunning = false;
+          this.isGameOver = true;
+          
+          this.snd_dead.play();
+
+          if (this.charaTweens != null) {
+            this.charaTweens.stop();
+          }
+
+          var myScene = this;
+
+          this.charaTweens = this.tweens.add({
+            targets: this.chara,
+            ease: "Elastic.easeOut",
+            duration: 750,
+            alpha: 0,
+            onComplete: function() {
+              myScene.gameOver();
+            }
+          });
         }
-      }
-    }
-
-    if (this.timerHalangan == 0) {
-      var acak_y = Math.floor(Math.random() * 680 + 60);
-
-      var halanganBaru = this.add.image(1500, acak_y, "obstc");
-
-      halanganBaru.setOrigin(0, 0);
-      halanganBaru.setData("status_aktif", true);
-      halanganBaru.setData("kecepatan", Math.floor(Math.random() * 15 + 10));
-
-      this.halangan.push(halanganBaru);
-
-      this.timerHalangan = Math.floor(Math.random() * 50 + 10);
-
-      halanganBaru.setDepth(3);
-    }
-
-    for (let i = this.halangan.length - 1; i >= 0; i--) {
-      this.halangan[i].x -= this.halangan[i].getData("kecepatan");
-
-      if (this.halangan[i].x < -200) {
-        this.halangan[i].destroy();
-
-        this.halangan.splice(i, 1);
-        break;
-      }
-    }
-
-    this.timerHalangan--;
-
-    for (var i = this.halangan.length - 1; i >= 0; i--) {
-      if (
-        this.chara.x > this.halangan[i].x + 50 &&
-        this.halangan[i].getData("status_aktif") == true
-      ) {
-        this.halangan[i].setData("status_aktif", false);
-        this.score++;
-        this.label_score.setText(this.score);
-      }
-    }
-
-    // Collision detection dengan obstacles
-    for (let i = this.halangan.length - 1; i >= 0; i--) {
-      if (
-        this.chara.getBounds().contains(this.halangan[i].x, this.halangan[i].y)
-      ) {
-        this.halangan[i].setData("status_aktif", false);
-        this.handleGameOver();
-        break;
-      }
-    }
-  },
-  
-  // Buat fungsi terpisah untuk handle game over
-  handleGameOver: function() {
-    this.isGameRunning = false;
-    this.isGameOver = true; // Set flag game over
-    
-    this.snd_dead.play();
-
-    if (this.charaTweens != null) {
-      this.charaTweens.stop();
-    }
-
-    var myScene = this;
-
-    this.charaTweens = this.tweens.add({
-      targets: this.chara,
-      ease: "Elastic.easeOut",
-      duration: 750,
-      alpha: 0,
-      onComplete: function() {
-        myScene.gameOver();
-      }
-    });
-  }
-});
+      });
